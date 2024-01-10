2f7fd0fce7d9d95e50e465c44f6e3a9375bf0a28
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2024-01-10 18:23:21 +0530

    completed the project

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
 
-type Props = {}
+/**
+ * eslint-disable @next/next/no-img-element
+ *
+ * @format
+ */
+
+/** @format */
+"use client";
+
+import { DataTable } from "@/components/DataTable";
+import { ColumnDef } from "@tanstack/react-table";
+import React from "react";
+import PageTitle from "@/components/PageTitle";
+import { cn } from "@/lib/utils";
+
+type Props = {};
+
+interface Setting {
+  category: string;
+  value: string | number | boolean;
+}
+
+const columns: ColumnDef<Setting>[] = [
+  {
+    accessorKey: "category",
+    header: "Category"
+  },
+  {
+    accessorKey: "value",
+    header: "Value"
+  }
+];
+const data: Setting[] = [
+  {
+    category: "Account",
+    value: true
+  },
+  {
+    category: "Notifications",
+    value: false
+  },
+  {
+    category: "Language",
+    value: "English"
+  },
+  {
+    category: "Theme",
+    value: "Dark"
+  }
+];
 
 export default function SettingsPage({}: Props) {
   return (
-    <div>SettingsPage</div>
-  )
+    <div className="flex flex-col gap-5  w-full">
+      <PageTitle title="Settings" />
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
